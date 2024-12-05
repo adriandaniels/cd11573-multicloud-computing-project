edac9296ce56ee2730510af038e42b3a4c5f89c0
Adrian Daniels <adrian_daniels@msn.com>
2024-12-05 13:52:26 -0500

    Replaced Terraform config for web app

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -1,6 +1,5 @@
 data "azurerm_resource_group" "udacity" {
   name       = "cloud-shell-storage-eastus"
-  location   = "canadaeast"
 }
 
 resource "azurerm_container_group" "udacity" {
@@ -58,35 +57,13 @@ resource "azurerm_mssql_server" "example" {
   }
 }
 
-# Addition: .NET Application
+# Addition: .NET Web Application
 
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "acctest-01"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+data "azurerm_windows_web_app" "example" {
+  name                = "existing"
+  resource_group_name = "existing"
 }
 
-resource "azurerm_container_app_environment" "example" {
-  name                       = "Example-Environment"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-}
-
-resource "azurerm_container_app" "example" {
-  name                         = "example-app"
-  container_app_environment_id = azurerm_container_app_environment.example.id
-  resource_group_name          = azurerm_resource_group.example.name
-  revision_mode                = "Single"
-
-  template {
-    container {
-      name   = "examplecontainerapp"
-      image  = "mcr.microsoft.com/k8se/quickstart:latest"
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-  }
+output "id" {
+  value = data.azurerm_windows_web_app.example.id
 }
